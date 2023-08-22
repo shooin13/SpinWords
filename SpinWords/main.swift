51d1d8ba3c365f8b1b5e1d6c099492a8e52639e6
Павел Николаев <shooin@MacBook-Pro.local>
2023-08-23 01:06:49 +0300

    Changes into initially printed instruction

--- SpinWords/main.swift
+++ SpinWords/main.swift
@@ -1,4 +1,4 @@
-print("Enter any string and it will reverse any word containing more than 4 characters")
+print("Enter any string consisting of several words and it will reverse any word containing more than 4 characters")
 
 var sentence = readLine()!
 
