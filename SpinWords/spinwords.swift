bb68ac234edc13db1982cafe6cbc7123302814b4
shooin13 <shooin13@gmail.com>
2023-08-23 22:20:45 +0300

    Update main.swift

--- SpinWords/spinwords.swift
+++ SpinWords/spinwords.swift
@@ -1,31 +1,9 @@
 func spinWords(in sentence: String) -> String {
-    let arr = sentence.split(separator: " ")
-    var preFinalArr: [String] = []
-    var finalStr = ""
-
-    for i in 0...arr.count-1 {
-        if arr[i].count >= 5 {
-            var charArr: [Character] = []
-            for char in arr[i] {
-                charArr.append(char)
-            }
-            var strToArr: String = ""
-            for element in 0...charArr.reversed().count-1 {
-                strToArr += String(charArr.reversed()[element])
-            }
-            preFinalArr.append(strToArr)
-        } else {
-            preFinalArr.append(String(arr[i]))
-        }
-    }
-    for i in 0...preFinalArr.count-1 {
-        if i == preFinalArr.count-1 {
-            finalStr += preFinalArr[i]
-        } else {
-            finalStr += preFinalArr[i] + " "
-        }
-    }
-return finalStr
+    sentence
+        .split(separator: " ")
+        .map { "\($0)" }
+        .map { $0.count > 4 ? String($0.reversed()) : $0 }
+        .joined(separator: " ")
 }
 
 
